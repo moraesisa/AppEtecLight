da56d050700b977b4a009f337e0c89ed64363a70
Usuario <Usuario@CPD03-M01>
2022-08-11 14:58:51 -0300

    Adicionar arquivos de projeto.

---
+++ AppEtecLight/AppEtecLight/MainPage.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Battery;
+using Xamarin.Essentials;
+
+
+namespace AppEtecLight
+{
+    public partial class MainPage : ContentPage
+    {
+        bool lanterna_ligada = false;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnOnOff.Source = ImageSource.FromResource("AppEtecLight.Images.botao-off.png");
+
+            Carrega_Informacoes_Bateria();
+
+        }
+
+        private async void Carrega_Informacoes_Bateria()
+        {
+            try
+            {
+                if (CrossBattery.IsSupported)
+                {
+                    CrossBattery.Current.BatteryChanged -= Mudanca_Status_Bateria;
+                    CrossBattery.Current.BatteryChanged += Mudanca_Status_Bateria;
+
+                } else
+                {
+                    lbl_bateria_fraca.Text = "As informações sobre a bateria não estão disponiveis :( ";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ocorreu um erro: \n", ex.Message, "OK");
+            }
+        }
+        private async void Mudanca_Status_Bateria(object sender, Plugin.Battery.Abstractions.BatteryChangedEventArgs e)
+        {
+            try
+            {
+                lbl_porcentagem_restante.Text = e.RemainingChargePercent.ToString() + "%";
+
+                if (e.IsLow)
+                {
+                    lbl_bateria_fraca.Text = "Atenção! A bateria está fraca!";
+                }
+
+                else
+                {
+                    lbl_bateria_fraca.Text = "";
+                }
+
+                switch (e.Status)
+                {
+                    case Plugin.Battery.Abstractions.BatteryStatus.Charging:
+                        lbl_status.Text = "Carregando";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.Discharging:
+                        lbl_status.Text = "Descarregando";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.Full:
+                        lbl_status.Text = "Carregada";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.NotCharging:
+                        lbl_status.Text = "Sem Carregar";
+                        break;
+
+                    case Plugin.Battery.Abstractions.BatteryStatus.Unknown:
+                        lbl_status.Text = "Desconhecido";
+                        break;
+                }
+
+            }
+            catch( Exception ex)
+            {
+                await DisplayAlert("Ocorreu um erro: \n ", ex.Message, "OK");
+            }
+        }    
+
+        private async void BtnOnOff_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!lanterna_ligada)
+                {
+                    lanterna_ligada = true;
+                    btnOnOff.Source = ImageSource.FromResource("AppEtecLight.Images.botao-on.png");
+                    Vibration.Vibrate(TimeSpan.FromMilliseconds(250));
+
+                    await Flashlight.TurnOnAsync();
+                }
+            
+                else
+                {
+                    lanterna_ligada=false;
+                    btnOnOff.Source = ImageSource.FromResource("AppEtecLight.Images.botao-o" +
+                        "ff.png");
+                    Vibration.Vibrate(TimeSpan.FromMilliseconds(250));
+
+                    await Flashlight.TurnOffAsync();
+                }
+            }
+                catch (Exception ex)
+                {
+                await DisplayAlert("Ocorreu um erro: \n ", ex.Message, "OK");
+                }
+            
+        }
+
+    }
+}
